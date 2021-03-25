55c49d9448236b26e727487515e3ff5bc8e96d48
Raiden Lin <rodes5292@gmail.com>
2021-03-25 11:04:12 +0800

    test tailwindcss

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -170,138 +170,24 @@ export default {
 </script>
 
 <template lang='pug'>
-
-
-.card(v-if='!props.bool')
-  .wordLeft
-    h2 Welcome Back
-    p To keep connect with up please login with your personal info
-    label(for="changePage") Sign In
-  .wordRight
-    h2 Hello Friend!
-    p Enter your personal details and start journey with us
-    label(for="changePage") Sign Up
-  form(action="/users/login" method="").loginPage  
-    h1 Log In
-    .group
-      label(for="email").far.fa-envelope
-      input(type="email" name="email" placeholder='Email' v-model='userEmail')
-    .group
-      label(for="password").fas.fa-key
-      input(type="password" name="password" placeholder='password' v-model='userPassword')
-    .group
-      button#login(type='submit' @click='handleSignIn') login
-      //- router-link(to="/editsheet") EditSheet
-      //- a.fab.fa-facebook(@click='facebookSignIn')
-      a.fab.fa-google-plus-g(@click='googleSignIn') G
-      //- router-link.fab.fa-google-plus-g(to="/editsheet" @click='googleSignIn') G
+.card.p-6.max-w-sm.mx-auto.rounded-xl.shadow-md.flex.items-center.space-x-4(v-if='!props.bool')
+  a(@click='googleSignIn') 疾管署 google帳號登入
         
-  form(action="/users/register" method="").registerPage
-    h1(v-if='!errorMsg') Create Account
-    h1(v-if='errorMsg') {{errorMsg}}
-    //- .group
-    //-   label(for="name").fas.fa-signature
-    //-   input(type="text" name="name" placeholder='name')
-    .group
-      label(for="email").far.fa-envelope
-      input#email(type="email" name="email" placeholder='Email' v-model='userEmail')
-    .group
-      label(for="password").fas.fa-key
-      input#pwd(type="password" name="password" placeholder='password' v-model='userPassword')
-    //- .group
-    //-   label(for="password2").fas.fa-key
-    //-   input(type="password" name="password2" placeholder=' confirm password')
-    button#submit(type='submit' @click='handleSignUp') Register
-  .cover(ref='cover')
-    button#changePage(name="changePage" @click='handleOpen')
+button.py-2.px-4.font-semibold.rounded-lg.shadow-md.text-white.bg-green-500(class='hover:bg-green-700')
+  | Click me
+
+
 
 </template>
 
 <style lang='stylus'>
-@import '../css/style.styl'
-$color_brown= #9b4b4b
-.card
-  box-shadow 2px 2px 4px rgba(0,0,0,0.5)
-  size(800px,480px,8px)
-  overflow hidden
-  position relative
+@css {
+  .card {
+    @apply rounded-lg bg-green-50 p-4
+  }
+
+}
+
   
-  .wordLeft,.wordRight
-    background-color transparent
-    flexCenter()
-    text-align center
-    height 200px
-    margin-top 100px
-    z-index 4
-    color #fff
-    position absolute
-    padding 16px
-    label
-      color $color_brown
-      font-weight 900
-      position absolute
-      margin-top 239px
-      
-  .wordLeft
-    width 300px
-    flex-direction column
-    margin-left -300px
-    // p
-    //   padding 16px
 
-  .wordRight
-    right 0
-    width 300px
-    flex-direction column
-   
-  .loginPage,.registerPage
-    color $color_brown
-    z-index 3
-    flexCenter()
-    flex-direction column
-    text-align center
-    position absolute
-    size(500px,480px,0px)
-    #login,#submit
-      margin-top 4px
-      size(100px,40px,16px)
-      border none
-      color #fff
-      background-color $color_brown
-      cursor pointer
-      
-    .fab.fa-facebook,.fab.fa-google-plus-g
-      flexCenter()
-      text-decoration none
-      margin-top 8px
-      font-size 24px
-      size(100px,40px,16px)
-      color #fff
-      background-color $color_brown
-      cursor pointer
-    .group
-      margin 8px
-      label
-        margin-right 4px
-  .registerPage
-    right 0
-    opacity 0
-
-  .cover
-    z-index 3
-    flexCenter()
-    flex-direction column
-    size(300px,480px,0px)
-    position absolute
-    right 0px
-    background-color $color_brown
-    color #fff
-    #changePage
-      position absolute
-      size(100px,40px,16px)
-      border none
-      margin-top 160px
-      color $color_brown
-      background-color #fff
-      font-size 20px
 </style>
